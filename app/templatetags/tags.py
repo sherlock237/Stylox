cda48b98ce205833ae481b26dcf6eb0dacae6547
PkSharma101 <pul22699@gmail.com>
2021-05-17 17:49:39 +0530

    password reset

---
+++ app/templatetags/tags.py
@@ -0,0 +1,14 @@
+from app.models import Product, Wishlist, MyProfile
+from django import template
+
+register = template.Library()
+
+@register.simple_tag
+def add_variable_to_context(request):  
+    obList = Wishlist.objects.filter(current_user = request.user.myprofile)
+    obno = obList.count()
+    return obno
+            
+
+        
+
--- app/urls.py
+++ app/urls.py
@@ -5,13 +5,19 @@ from . import views
 from django.conf.urls.static import static
 from django.urls.conf import include
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
 
+
     path('',views.IndexView.as_view(), name="index"),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="registration/password_reset_confirm.html"), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),      
     path("password_reset", views.password_reset_request, name="password_reset"),
     path('add_wishlist/<int:pk>', views.add_to_wishlist),
     path('remove_wishlist/<int:pk>', views.remove_from_wishlist),
+    path('remove_table_wishlist/<int:pk>', views.remove_from_table_wishlist),
     path('wishlist/', views.WishlistView.as_view()),
     path('single-product', views.singleproduct),
     path('checkout', login_required(views.checkoutview.as_view(success_url= ''))),
